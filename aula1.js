ff3563e6a1924003ee5fe2cb04d33897d74dfeb3
RenanG18 <renangsrodrigues@gmail.com>
2022-11-25 10:42:53 -0300

    Versão 2.0

--- aula1.js
+++ aula1.js
@@ -1,10 +1,11 @@
 var http = require('http');
-
+var dia = require('./biblioteca')
 http.createServer(function (req, res){
 
     res.writeHead(200, {'Content-Type':'text/html' });
-    res.write('<h2>Brasil 2x0 Servia</h2>');
+    res.write('<h2 align = center>Brasil 2x0 Servia</h2>');
     res.write(Date().substring(16, 24)+'<br>');
-    res.end('Uma pena....');
+    res.write('<p align = center >'+dia.diaDaSemana()+'</p>'+'<BR>');
+    res.end('<p style=color:green> Uma pena....</p>');
 
 }).listen(3000);
