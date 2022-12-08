82b349273b0577594727a060281b83ee15c2b87d
RenanG18 <renangsrodrigues@gmail.com>
2022-12-08 10:42:37 -0300

    Comando básico para criar arquivo na 3° aula

--- criararquivo.js
+++ criararquivo.js
@@ -1,8 +1,8 @@
 var fs = require("fs");
 
 
-fs.appendFile( 'novoArquivo.txt', ' Texto: Vai aparecer dentro do arquivo', function(err){
+fs.appendFile( 'novoArquivo.txt', 'Texto: Vai aparecer dentro do arquivo', function(err){
 if(err) throw err;
-console.log('Arquivo criado com sucesso');
+console.log('Arquivo criado com sucesso!');
 
 } );
