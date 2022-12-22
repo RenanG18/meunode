0f3252ad105e0eb8145521b295d6badfb0c4c696
RenanG18 <112626751+RenanG18@users.noreply.github.com>
2022-12-22 16:40:28 -0300

    comandos para criar arquivos de texto 3º aula

--- arquivos.js
+++ arquivos.js
@@ -8,6 +8,7 @@ var q = url.parse(req.url, true).query;
 res.write('Arquivo criado com sucesso!');
 fs.appendFile(q.nome+'.txt', q.conteudo, function(err){
 if(err)throw err;
+  
 });
 res.end();
 }).listen(80);
